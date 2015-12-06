23fec0d06283adb415f43c21b84f37ec8ee8baae
Vivek <vsdutt@gmail.com>
2015-12-06 17:01:46 +0530

    Menu
    Adding add menu screen

--- www/js/app.js
+++ www/js/app.js
@@ -58,6 +58,14 @@ angular.module('starter', ['ionic', 'starter.controllers', 'veg.items', 'veg.ord
       }
     }
   })
+  .state('app.addMenu', {
+     url: "/addMenu",
+     views: {
+       'menuContent': {
+         templateUrl: "templates/addMenu.html"
+       }
+     }
+   })
   ;
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/veg');
